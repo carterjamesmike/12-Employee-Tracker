7b5650fa46b348373cb3942ef3ff8033d52a7586
James Carter <carterjamesmike@gmail.com>
2022-11-30 22:32:14 -0600

    Initial commit with framework for server, index.js, and db's

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL, 
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+);
+
+--Revisit primary keys, foreign keys, and references, and set null when delete 
