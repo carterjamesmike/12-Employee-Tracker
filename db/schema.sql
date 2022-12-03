caadc682a5826c827225c5d19340659c2b00e344
James Carter <carterjamesmike@gmail.com>
2022-12-02 19:03:16 -0600

    Added required packages, removed server.js, updated schema.db, and added working to do list on index.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,26 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL, 
-    name VARCHAR(30),
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    department_name VARCHAR(30),
 );
 
-CREATE TABLE role (
-    id INT,
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (role_id)
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES (roles(id))
+    ON DELETE SET NULL
 );
-
---Revisit primary keys, foreign keys, and references, and set null when delete 
--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Required Prompts
+//On start up
+//  View all departmeents  
+//  View all roles
+//  View all employees
+//  Add a department
+//  add a role
+//  add an employee
+//  update an employee
+
+//View all departments
+//  Tables showing department names and ids
+
+//View all roles
+//  Table showing job title, role id, depatarment, salary
+
+//View employees
+//  Table showing employee id, firt name, last name job title, dept, salary, and manager the employee reports to
+
+//Add department 
+//  Prompted to enter the name of department and its added to database (show all departments after)
+
+//Add role
+//  Prompted to enter name, salary, and department of role and is added to db (show roles after)
+
+//Add employee 
+//  Prompted to enter firt name, last name, role, and manager and new employee is added to database (show employees afterwards)
+
+//Bonus
+//Update employee managers
+//view employees by manager
+//view employees by department
+//Delete departments, roles, employees
+//View total utilized budged of a department (combined salaries of all employees in dept)
+
